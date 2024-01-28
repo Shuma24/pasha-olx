f8c9d10e11dc57844a665c67b3e60c13d639768b
Shuma24 <pavloshuma@gmail.com>
2024-01-28 13:05:22 +0200

    fix-route

--- api/src/shared/api/api-instance.ts
+++ api/src/shared/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 export const apiInstance = axios.create({
-  baseURL: 'http://localhost:8888',
+  baseURL: 'http://backend:8888',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
