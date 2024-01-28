d513ba43958df23eb0d29c0cf6f9af36ae4347e8
Shuma24 <pavloshuma@gmail.com>
2024-01-28 17:34:52 +0200

    cors settings

--- api/src/shared/api/api-instance.ts
+++ api/src/shared/api/api-instance.ts
@@ -1,6 +1,14 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
-export const clearApiInstance = axios.create();
+export const clearApiInstance = axios.create({
+  headers: {
+    'Access-Control-Allow-Origin': 'http://3.126.152.164',
+    Accept: '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    Connection: 'keep-alive',
+    'Content-Type': 'application/json',
+  },
+});
 
 export const apiInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
