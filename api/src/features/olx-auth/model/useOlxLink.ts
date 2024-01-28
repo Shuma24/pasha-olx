a0edef8359987636d0249e84cb6c9759d42f9124
Shuma24 <pavloshuma@gmail.com>
2024-01-28 15:59:06 +0200

    fix env

--- api/src/features/olx-auth/model/useOlxLink.ts
+++ api/src/features/olx-auth/model/useOlxLink.ts
@@ -1,7 +1,7 @@
 export function useOlxAuthLink() {
   const olxUrl = new URL('https://www.olx.ua/oauth/authorize');
-  olxUrl.searchParams.append('client_id', process.env.CLIENT_ID as string);
-  olxUrl.searchParams.append('redirect_uri', process.env.CLIENT_SECRET as string);
+  olxUrl.searchParams.append('client_id', process.env.NEXT_PUBLIC_CLIENT_ID as string);
+  olxUrl.searchParams.append('redirect_uri', process.env.NEXT_PUBLIC_CLIENT_SECRET as string);
   olxUrl.searchParams.append('response_type', 'code');
 
   const handleClick = () => {
